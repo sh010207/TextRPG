d319424b326e6329664acf6b301f7e029509eac6
sh010207 <sh010207@naver.com>
2024-09-26 17:28:23 +0900

    Monster
    -Goblin, Slime, Oak 체력, 이름 공격력 할당

---
+++ TextRPG/Monster.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextRPG
+{
+    public interface IMonster
+    {
+        string Name { get; }
+        int Hp { get; set; }
+        int Atk { get; }
+        bool IsDead { get; }
+    }
+
+    //몬스터 클래스
+    public class Monster : IMonster
+    {
+        public string Name { get; }
+        public int Hp { get; set; }
+        public int Atk { get; set; }
+        public bool IsDead =>  Hp <=0;
+
+        public Monster(string name, int hp, int atk) 
+        {
+            Name = name;
+            Hp = hp;
+            Atk = atk;
+        }
+    }
+
+    public class Slime : Monster
+    {
+        public Slime(string name) : base(name, 10, 5) { }   
+    public class Goblin : Monster
+    {
+        public Goblin(string name) : base(name, 15, 7) { }
+    }
+    public class Oak : Monster
+    {
+        public Oak(string name) : base (name, 25, 10) {
+    }
+}
