1521f1afeef715562920067115ca3646378a7692
Hongshinyoung <88022654+Hongshinyoung@users.noreply.github.com>
2024-09-26 17:28:34 +0900

    기능추가
    Feat: 게임메니저, 플레이어 클래스 업로드

---
+++ TextRPG/Player.cs
@@ -0,0 +1,24 @@
+﻿namespace TextRPG
+{
+    public class Player
+    {
+        public string name { get; set; }
+        public int level {  get; set; }
+        public string job { get; set; }
+        public int ad {  get; set; }
+        public int df { get; set; }
+        public int hp { get; set; }
+        public int gold { get; set; }
+
+        public Player(string name, int level, string job, int ad, int df, int hp, int gold)
+        {
+            this.name = name;
+            this.level = level;
+            this.job = job;
+            this.ad = ad;
+            this.df = df;
+            this.hp = hp;
+            this.gold = gold;
+        }
+    }
+}
