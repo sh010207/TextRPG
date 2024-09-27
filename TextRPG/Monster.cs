fa012ce9b3f8760402230553d44b86eaa800a333
sh010207 <sh010207@naver.com>
2024-09-27 15:37:20 +0900

    몬스터 랜덤생성 및 AtkUI구성(기능x)
    -고블린,슬라임,오크 랜덤 생성
    -AtkUI 구현 (기능x)
    [Bug] -AtkUI 들어가게되면 몬스터의 인덱스가 이상해지고 몬스터가 초기화됨
    (수정할 예정)

--- TextRPG/Monster.cs
+++ TextRPG/Monster.cs
@@ -2,24 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace TextRPG
 {
-    public interface IMonster
-    {
-        int Lv { get;}
-        string Name { get; }
-        int Hp { get; set; }
-        int Atk { get; }
-        bool IsDead { get; }
-    }
-
     //몬스터 클래스
-    public class Monster : IMonster
+    public class Monster 
     {
-        public int Lv { get;}
-        public string Name { get; }
+        public int Lv { get; set; }
+        public string Name { get; set; }
         public int Hp { get; set; }
         public int Atk { get; set; }
         public bool IsDead =>  Hp <= 0;
@@ -32,18 +24,5 @@ namespace TextRPG
             Hp = hp;
             Atk = atk;
         }
-    }
-
-    public class Slime : Monster
-    {
-        public Slime(string name) : base(2,name, 10, 5) { }
-    }
-    public class Goblin : Monster
-    {
-        public Goblin(string name) : base(5,name, 15, 7) { }
-    }
-    public class Oak : Monster
-    {
-        public Oak(string name) : base(7,name, 25, 10) { }
     }
 }
