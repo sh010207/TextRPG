7d08fe8cb6e4e3e2ff89cad08c043323389e91a5
omyman12 <qkrrlcks00@gmail.com>
2024-09-27 16:02:42 +0900

    item
    item

---
+++ TextRPG/Item.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextRPG
+{
+    internal class Item
+    {
+        public string itemName { get; }
+        public int itemType { get; }
+        public int itemValue { get; }
+        public string itemDesc { get; }
+        public int itemPrice { get; }
+        public string DisplayTypeText
+        {
+            get
+            {
+                return itemType == 0 ? "공격력" : "방어력";
+            }
+        }
+
+        public Item(string itemname, int itemtype, int itemvalue, string itemdesc, int itemprice)
+        {
+            itemName = itemname;
+            itemType = itemtype;
+            itemValue = itemvalue;
+            itemDesc = itemdesc;
+            itemPrice = itemprice;
+        }
+       public string ItemInfoText()
+        {
+            return ($"{itemName} | {DisplayTypeText} +{itemValue} | {itemDesc} ");
+        }
+    }
+}
