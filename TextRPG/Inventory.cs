d4e8906fb55ca661cd4f48fe49ca13929ea6e554
Hongshinyoung <88022654+Hongshinyoung@users.noreply.github.com>
2024-10-01 16:25:36 +0900

    2차 수정
    - inventory 수정 완
    - 우파루파 수정
    - 상점 UI 완
    - player, gamemanager 조금 수정

--- TextRPG/Inventory.cs
+++ TextRPG/Inventory.cs
@@ -26,9 +26,9 @@ namespace TextRPG
             Console.WriteLine("인벤토리");
             Console.WriteLine("보유중인 아이템을 관리할 수 있습니다.");
             Console.WriteLine();
-            Console.WriteLine("[ 아이템 목록 ]");
+            Console.WriteLine("[ 아이템 목록 ]\n");
 
-            player.ShowInventory(false);
+            ShowInventory(false);
 
             Console.WriteLine();
             Console.WriteLine("1 : 장착 관리");
@@ -53,15 +53,53 @@ namespace TextRPG
 
         public void DisplayEquipUI() //장착 할래말래?
         {
+            Console.Clear();
+            List<Item> items = new List<Item>();
+            Console.WriteLine("인벤토리 - 장착 관리");
+            Console.ResetColor();
+            Console.WriteLine("보유 중인 아이템을 관리할 수 있습니다.\n");
+            Console.WriteLine("[아이템 목록]\n");
+            ShowInventory(true); // 아이템 목록 표시 ( 인덱스 o 누르면 장착);
 
+            Console.WriteLine("\n0. 나가기\n원하시는 행동을 입력해주세요.");
+            int result = GameManager.SelectBehavior(0, player.InventoryCount);
+
+            switch (result)
+            {
+                case 0:
+                    GameManager.GameStartUI();
+                    break;
+                default:
+                    int itemIndex = result - 1;
+                    Item targetItem = player.returnInventory[itemIndex];
+                    player.EquipItem(targetItem);
+                    DisplayEquipUI();
+                    break;
+            }
+
+            //public void Add()  // 인벤에 아이템 추가
+            //{
+            //    // 아이템 인벤에 추가해주는 코드
+            //}
         }
 
-        public void Add()  // 인벤에 아이템 추가
+        public void ShowInventory(bool ItemIndex) // 인벤 보여주기
         {
-            // 아이템 인벤에 추가해주는 코드
+            for (int i = 0; i < player.InventoryCount; i++)
+            {
+                Item items = player.Inventory[i];
+
+                string ShowItemIndex = ItemIndex ? $"{i + 1}" : "";
+                string ShowEquipItems = player.IsEquipped(items) ? $"[E]" : ""; // class Player에서 Player가 장착되어있는지 확인 / 되어있다면 [E]출력 아니면 공백
+                Console.WriteLine($"- {ShowItemIndex} {ShowEquipItems} {items.ItemInfoText()}"); // -번호 [E] | 아이템 설명
+            }
+            if (player.InventoryCount == 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("\n   [ 보유한 아이템이 없습니다.]      \n\n");
+                Console.ResetColor();
+            }
         }
-
-
     }
 }
 
--- TextRPG/Player.cs
+++ TextRPG/Player.cs
@@ -14,11 +14,11 @@ namespace TextRPG
         public int maxhp { get; set; }
         public int hp { get; set; }
         public int gold { get; set; }
-        public int extraAd {  get; set; }
-        public int extraDf { get; set; }
+        public int extraAd {  get; private set; }
+        public int extraDf { get; private set; }
 
-        private List<Item> equipItems = new List<Item>();
-        private List<Item> Inventory = new List<Item>();
+        public List<Item> equipItems = new List<Item>();
+        public List<Item> Inventory = new List<Item>();
 
         private static Item item;
 
@@ -28,6 +28,15 @@ namespace TextRPG
             {
                 return Inventory.Count;
             }
+        }
+
+        public List<Item> returnInventory //인벤토리 개수 받아오기
+        {
+            get
+            {
+                return Inventory;
+            }
+
         }
 
         public Player(string name, int level, string job, int ad, int df, int maxhp, int hp, int gold)
@@ -46,45 +55,33 @@ namespace TextRPG
         {
             Console.WriteLine($"Lv. {level:D2}");
             Console.WriteLine($"{name} ( {job} )"); ////Cha 하드코딩되어있어서 이름으로 변경
-            Console.WriteLine(extraAd == 0 ? $"공격력 {ad}" : $"공격력 {ad} + ( {extraAd} )");
-            Console.WriteLine(extraDf == 0 ? $"방어력 {df}" : $"방어력{df} + ( {extraDf} )");
+            Console.WriteLine(extraAd == 0 ? $"공격력 {ad}" : $"공격력 {extraAd + ad} ( + {extraAd} )");
+            Console.WriteLine(extraDf == 0 ? $"방어력 {df}" : $"방어력 {extraDf + df} ( + {extraDf} )");
             Console.WriteLine($"체 력 {hp}/{maxhp}");
             Console.WriteLine($"Gold {gold}");
         }
 
-        public void ShowInventory(bool ItemIndex) // 인벤 보여주기
+        public void ChangeJob(string job, int ad, int df, int maxhp, int hp, int gold) //직업선택을 위한
         {
-            for (int i = 0; i < InventoryCount; i++)
-            {
-                Item items = Inventory[i];
-
-                string ShowItemIndex = ItemIndex ? $"{i + 1}" : "";
-                string ShowEquipItems = IsEquipped(items) ? $"[E]" : ""; // class Player에서 Player가 장착되어있는지 확인 / 되어있다면 [E]출력 아니면 공백
-                Console.WriteLine($"-{ShowItemIndex} {ShowEquipItems} |  {items.ItemInfoText}"); // -번호 [E] | 아이템 설명
-            }
-            if(InventoryCount == 0)
-            {
-                Console.WriteLine("   [보유한 아이템이 없습니다.]      \n\n");
-            }    
+            this.job = job; 
+            this.ad = ad;
+            this.df = df;
+            this.maxhp = maxhp;
+            this.hp = hp;
+            this.gold = gold;
         }
-
-        //public void InventoryInfo()
-        //{
-
-
-        //}
 
         //아이템 장착 [E]
         public void EquipItem(Item item)
         {
-            if(IsEquipped(item))         // 아이템 장착이 되어있다면
+            if (IsEquipped(item))         // 아이템 장착이 되어있다면
             {
                 equipItems.Remove(item);    // 장비아이템에서 아이템을 빼준다.
                 if (item.itemType == 0)     // 빼준아이템 타입이 0이면
                 {
                     extraAd -= item.itemValue;    //  표시된 무기 공격력에서 무기공격력만큼 빼준다.
                 }
-                if(item.itemType == 1)    //  타입이 1이면
+                if (item.itemType == 1)    //  타입이 1이면
                 {
                     extraDf -= item.itemValue;  // 표시된 방어구 방어력에서 방어구방어력만큼 빼준다.
                 }
@@ -92,11 +89,11 @@ namespace TextRPG
             else                         // 장착이 안되어있다면
             {
                 equipItems.Add(item);
-                if(item.itemType == 0)
+                if (item.itemType == 0)
                 {
                     extraAd += item.itemValue;
                 }
-                if(item.itemType == 1)
+                if (item.itemType == 1)
                 {
                     extraDf += item.itemValue;
                 }
